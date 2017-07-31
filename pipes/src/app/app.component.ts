cfb9d009351757160de4b4dedf6ad2087b7b31a0
Tomas muniz <palanx67.store@gmail.com>
2017-07-31 15:46:04 -0400

    Built-in pipes con ejemplos

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre:string = "Tomás Muñiz";
+
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9,10];
+
+  PI:number = Math.PI;
+
+  percent:number = 0.245;
+
+  salario:number = 650000.25;
+
+  heroe:any = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve, reject)=>{
+    setTimeout( ()=>resolve('Llego la data!'), 3500 );
+  } );
+
+  fecha:Date = new Date(2017,6,31);
 }
