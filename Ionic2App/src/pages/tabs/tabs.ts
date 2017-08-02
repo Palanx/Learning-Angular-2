a3c1d62c28e677b76327e92dd1dc5e9f70e429ec
Tomas muniz <palanx67.store@gmail.com>
2017-08-02 18:20:49 -0400

    Zip incorporado al proyecto

--- Ionic2App/src/pages/tabs/tabs.ts
+++ Ionic2App/src/pages/tabs/tabs.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { PendientesComponent } from '../pendientes/pendientes.component';
 import { TerminadosComponent } from '../terminados/terminados.component';
 
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  tab1Root = PendientesComponent;
-  tab2Root = TerminadosComponent;
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  tab1Root: any = PendientesComponent;
+  tab2Root: any = TerminadosComponent;
 
   constructor() {
 
