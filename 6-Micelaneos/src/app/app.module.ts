63cf8ec5b27ee20aded7bd9d65e8c93f620c74f0
Tomás Muniz <tmunizq@gmail.com>
2018-08-01 15:17:56 -0400

    micelaneos: rutas

--- 6-Micelaneos/src/app/app.module.ts
+++ 6-Micelaneos/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
@@ -8,6 +10,12 @@ import { ClasesComponent } from './components/clases/clases.component';
 import { AsyncComponent } from './components/async/async.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
 import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario-nuevo/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario-editar/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario-detalle/usuario-detalle.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +25,17 @@ import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
     ClasesComponent,
     AsyncComponent,
     ResaltadoDirective,
-    NgSwitchComponent
+    NgSwitchComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
