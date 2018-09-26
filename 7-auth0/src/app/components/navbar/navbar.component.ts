dd0bbbdea77da7aeec5163a2cce33faeaaf9dcad
palanx <tmunizq@gmail.com>
2018-09-26 17:14:20 -0300

    auth0: ya se puede loguear

--- 7-auth0/src/app/components/navbar/navbar.component.ts
+++ 7-auth0/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
 
-  constructor() { }
+  constructor(
+    private _auth0: AuthService
+  ) { }
 
-  ngOnInit() {
+  login(){
+    this._auth0.login();
+  }
+
+  logout(){
+    this._auth0.logout();
   }
 
 }
