163e150c1d35f119be7c57a20604549a52a15689
Tomas muniz <palanx67.store@gmail.com>
2018-10-14 02:49:45 -0300

    forms: creando el componente template y agregando ngmodel a los input

--- 8-forms/src/app/app.module.ts
+++ 8-forms/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { DateComponent } from './components/date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    DateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
