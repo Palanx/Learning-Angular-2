34e83b7393e20afb4147c8fad5bc1367c73b2815
palanx <tmunizq@gmail.com>
2018-10-16 15:18:15 -0300

    forms: enlazar inputs al form

--- 8-forms/src/app/components/data/data.component.ts
+++ 8-forms/src/app/components/data/data.component.ts
@@ -12,9 +12,9 @@ export class DataComponent{
 
   constructor() { 
     this.forma = new FormGroup({
-      'nombre': new FormControl('Tomás'),
-      'apellido': new FormControl(''),
-      'correo': new FormControl('')
+      'nombre': new FormControl('Tomás', Validators.required),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
   }
 
