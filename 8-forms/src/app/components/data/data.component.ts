f38b8f61e486c177d87ecdef07a0adaedd7277f4
palanx <tmunizq@gmail.com>
2018-10-16 15:53:16 -0300

    forms: reset y carga de data al form a partir de un objeto

--- 8-forms/src/app/components/data/data.component.ts
+++ 8-forms/src/app/components/data/data.component.ts
@@ -10,7 +10,7 @@ export class DataComponent{
 
   forma: FormGroup;
 
-  usuario: Object = {
+  usuario: any = {
     nombrecompleto: {
       nombre: "Tomás",
       apellido: "Muñiz"
@@ -21,15 +21,20 @@ export class DataComponent{
   constructor() { 
     this.forma = new FormGroup({
       'nombrecompleto': new FormGroup({
-        'nombre': new FormControl('Tomás', [Validators.required, Validators.minLength(3)]),
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
         'apellido': new FormControl('', Validators.required)
       }),
       'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
+
+    this.forma.setValue(this.usuario); //Si el objeto tiene la misma estructura que el FormGroup se setean los valores
   }
 
 
   guardarCambios(){
     console.log("value",  this.forma.value);
+
+    this.forma.reset( this.usuario ); // Resetea el form quedando pristine y lo deja limpio o le bindea un objeto como en este caso
+    this.forma.controls.correo.setValue("Formulario enviado"); // Setear valores directamente al control
   }
 }
