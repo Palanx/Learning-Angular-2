f1d79e3c088eb7f7382ef05d040a719d1bc52406
palanx <tmunizq@gmail.com>
2018-10-16 15:41:45 -0300

    forms: formgroup anidados

--- 8-forms/src/app/components/data/data.component.ts
+++ 8-forms/src/app/components/data/data.component.ts
@@ -20,8 +20,10 @@ export class DataComponent{
 
   constructor() { 
     this.forma = new FormGroup({
-      'nombre': new FormControl('Tomás', [Validators.required, Validators.minLength(3)]),
-      'apellido': new FormControl('', Validators.required),
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('Tomás', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required)
+      }),
       'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
   }
