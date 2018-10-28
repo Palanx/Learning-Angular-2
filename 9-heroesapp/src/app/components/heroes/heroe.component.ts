386f0365930121d8724eeb940ae8d1129e1c0bdf
Tomas muniz <palanx67.store@gmail.com>
2018-10-28 15:10:31 -0300

    heroesapp: http post firebase

--- 9-heroesapp/src/app/components/heroes/heroe.component.ts
+++ 9-heroesapp/src/app/components/heroes/heroe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
 
 export interface Heroe {
   key$?: string,
@@ -22,13 +24,25 @@ export class HeroeComponent implements OnInit {
     bio: ''
   }
 
-  constructor() { }
+  constructor(
+    private _heroe: HeroesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
   submit(){
     console.log(this.forma);
+
+    this._heroe.nuevoHeroe( this.heroe ).subscribe(
+      res => {
+        this.router.navigate(['/heroe', res.name]);
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
 }
--- 9-heroesapp/src/app/services/heroes.service.ts
+++ 9-heroesapp/src/app/services/heroes.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Heroe } from '../components/heroes/heroe.component';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  constructor() { }
+  fireURL: string = 'https://heroesapp-30897.firebaseio.com//heroes.json';
+
+  constructor(
+    private http: Http
+  ) { }
+
+  nuevoHeroe( heroe:Heroe ){
+
+    let body = JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type':'application-json'
+    });
+
+    return this.http.post( this.fireURL, body, {headers} ).pipe(
+      map( res => { 
+        console.log(res.json());
+        return res.json();
+      } )
+    );
+
+  }
 }
